a8e8117769ee9162f923b524f2c289fb9d674162
Sakshijhamnani <sakshijhamnani@gmail.com>
2024-01-10 14:03:54 +0530

    created new components

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
+import ExpenseDetails from './ExpenseDetails'
 
 function ExpenseItem(props) {
+   
    
   return (
    
       <div  className='expense-item'>
-        <div>{props.date.toISOString()}</div>
-        <div className='expense-item__description'>
-            <h2>{props.title}</h2>
-            <h4>{props.locationOfExpenditure}</h4>
-            <div className='expense-item__price' >${props.amount}</div>
-        </div>
+           <ExpenseDate date={props.date}/>
+           <ExpenseDetails amount={props.amount}  locationOfExpenditure={props.locationOfExpenditure}   title={props.title} />
       </div>
     
   )
