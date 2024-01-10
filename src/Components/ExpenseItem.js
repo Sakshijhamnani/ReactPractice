2025d7d8382fe013019462bae45310e1373af7dc
Sakshijhamnani <sakshijhamnani@gmail.com>
2024-01-10 09:20:30 +0530

    Created First Component

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense Items</h2>
+      <p>Food Rs.10</p>
+      <p>Petrol Rs.10</p>
+      <p>Movies Rs.10</p>
+    </div>
+  )
+}
+
+export default ExpenseItem
